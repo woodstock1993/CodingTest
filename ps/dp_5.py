c68ea7de37ceb07c590f78cc1cf898c12db57e27
woodstock1993 <diqkqkqk@gmail.com>
2022-06-04 11:08:42 +0900

    (add)피보나치

---
+++ ps/dp_5.py
@@ -0,0 +1,11 @@
+def fibonacci():
+    m = int(input())
+
+    dp = [0] * (m + 1)
+
+    dp[0] = 0
+    dp[1] = 1
+    for i in range(2, m + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return dp[m]
+
