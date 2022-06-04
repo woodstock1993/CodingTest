97ccb0b0deb56b18ade1cccf077477855bbcabd1
woodstock1993 <diqkqkqk@gmail.com>
2022-06-04 13:54:49 +0900

    (fix)1만들기

--- ps/dp_6.py
+++ ps/dp_6.py
@@ -1,19 +1,12 @@
-def dp():
-    m = int(input())
-    dp = [0]*(m+3)
-    dp[1] = 0
-    dp[2] = 1
-    dp[3] = 1
-    if m == 1:
-        return 0
-    elif m == 2 or m == 3:
-        return 1
+def dp(m):
+    dp = [0]*1000001
 
-    for i in range(4, m+1):
+    for i in range(2, m+1):
         dp[i] = dp[i-1]+1
         if i % 2 == 0:
-            dp[i] = min(dp[i//2], dp[i-1]) + 1
+            dp[i] = min(dp[i//2]+1, dp[i])
         if i % 3 == 0:
-            dp[i] = min(dp[i//3], dp[i-1]) + 1
+            dp[i] = min(dp[i//3]+1, dp[i])
 
     return dp[m]
+
